a724dab241585fde2f2db27da3090cf55e4064d7
LibXZR <i@xzr.moe>
2022-02-16 17:41:50 +0800

    kebab: Kscope-ify
    Change-Id: I32202fdab26c41568835b4c9702e2704caf0aa9f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kebab.mk
+    $(LOCAL_DIR)/kscope_kebab.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_kebab-user \
-    lineage_kebab-userdebug \
-    lineage_kebab-eng
+    kscope_kebab-user \
+    kscope_kebab-userdebug \
+    kscope_kebab-eng
--- kscope_kebab.mk
+++ kscope_kebab.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/kscope/config/mobile.mk)
 
-PRODUCT_NAME := lineage_kebab
+PRODUCT_NAME := kscope_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
